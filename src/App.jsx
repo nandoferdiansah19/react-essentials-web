ced53109b37e688097d8eacb78021136cbdd30ca
Nando Ferdiansah <nandoferdiansah19@gmail.com>
2024-11-23 05:37:13 +0700

    Added React Essentials Website Project

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     setSelectedTopic(e.target.textContent.toLowerCase());
   };
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -65,7 +65,7 @@ function App() {
           </div>
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,14 @@ const concepts = ["Fundamental", "Core", "Crucial"];
 const random = Math.floor(Math.random() * 3);
 
 const Header = () => {
+  const description = concepts[random];
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {concepts[random]} React concepts you will need for almost any app you
-        are going to build!
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
   );
